ca2f67845c081813ebe31c6159cfa82ec1bacedf
Samba <sambasivam.k@gmail.com>
2024-02-01 22:58:16 +0530

    Refactoring_3: Changed struct method to return Result, Handled error in main for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{env, fs, process};
 fn main() {
 
     let args: Vec<String> = env::args().collect();
@@ -10,7 +10,10 @@ fn main() {
     // let query_string = &args[1];
     // let file_path    = &args[2];
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument: {err}");
+        process::exit(1);
+    });
 
     // println!("The search string is {} in the file path {}", query_string, file_path);
 
@@ -26,11 +29,28 @@ struct Config<'a> {
 }
 
 impl Config <'_>{
-    fn new(args: &[String]) -> Config{
+    // fn new(args: &[String]) -> Config {
+
+    //     if args.len() < 3 {
+    //         return Err("Insufficient arguments provided")
+    //     }
+
+    //     let query_string = &args[1];
+    //     let file_path    = &args[2];
+
+    //     Config { query_string, file_path}
+
+    // }
+    fn build(args: &[String]) -> Result<Config, &str>{
+
+        if args.len() < 3 {
+            return Err("Insufficient arguments provided")
+        }
+
         let query_string = &args[1];
         let file_path    = &args[2];
 
-        Config { query_string, file_path}   
+        Ok(Config { query_string, file_path})   
 
     }
 }
