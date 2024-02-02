dbb402a2c78d06eeb0389d8c9b817b26c6168ed7
Samba <sambasivam.k@gmail.com>
2024-02-02 18:52:35 +0530

    Refactoring_7: added error to stderror

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {err}");
+        eprintln!("Problem parsing argument: {err}");
         process::exit(1);
     });
 
     let result = run(config);
     if let Err(e) = result {
-        println!("Application error occured {}", e);
+        eprintln!("Application error occured {}", e);
         process::exit(1);
     } else {
         for item in result.unwrap() {
